032071081b3ab944c468e7e04edb49a4437e959d
Aronja <anovicky@thoughtworks.com>
2020-04-30 21:50:57 +0100

    Add jetty deps and create simple handler function

--- src/clojure_service/core.clj
+++ src/clojure_service/core.clj
@@ -1,6 +1,15 @@
-(ns clojure-service.core)
+(ns clojure-service.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body  "<h1>Hello Clojure!</h1>"})
+   :body "Hello world"})
+
+  (defn -main
+        "I don't do much yet"
+        [& args]
+        (println "Hello Clojure")
+    (run-jetty handler {:port 8080}))
+
+
